d0bac5fb0d2d95addbe2f58e281c909e8c0d7c00
akshay <akshayjunghare10@gmail.com>
2019-08-01 21:24:42 +0530

    Datatype commit

---
+++ BasicCoreJava/src/vairiablesanddatatypes/DataTypes.java
@@ -0,0 +1,35 @@
+package vairiablesanddatatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		// Integer value
+int age=30;
+System.out.println(age);
+
+//Character value
+
+char gender='M';
+
+//Decimal value
+float grade =8.7f;
+double mrks=3.6;
+
+//Boolean value
+boolean status=true;
+
+//Strings
+String name="pooja";
+
+//For Integer values
+
+byte a=10;
+short b=20;
+int c=30;
+long d=40;
+
+
+	}
+
+}
