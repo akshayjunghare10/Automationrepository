ce10777d8f6b65436d19f66968abc0c53cd9bb38
akshay <akshayjunghare10@gmail.com>
2019-08-01 23:42:38 +0530

    Addition commit

---
+++ BasicCoreJava/src/vairiablesanddatatypes/Addition.java
@@ -0,0 +1,17 @@
+package vairiablesanddatatypes;
+
+public class Addition {
+	public static void main(String[] args) {
+	
+	int a=10;
+	int b=20;
+	int c= a+b;
+	int d=a-b;
+	
+	System.out.println("addition :"+c);
+	System.out.println("subtrion :"+d);
+			
+}
+	
+	
+}
