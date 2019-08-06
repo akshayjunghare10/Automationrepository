1c54bfb3a8535f719d448e949a79e9bb82950e4f
akshay <akshayjunghare10@gmail.com>
2019-08-06 20:06:07 +0530

    conditionalstatement commit

---
+++ BasicCoreJava/src/vairiablesanddatatypes/ConditionalStatement.java
@@ -0,0 +1,19 @@
+package vairiablesanddatatypes;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if(a>b)
+		{
+			System.out.println("a is greater than b");
+		}
+		else
+		{
+			System.out.println("a is not grater than b");
+		}
+
+	}
+
+}
