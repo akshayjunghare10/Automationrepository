db522c412a5a528971f791e32139fcc457634d4f
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:53:22 +0530

    elseif stament commit

---
+++ BasicCoreJava/src/vairiablesanddatatypes/ElseifStatement.java
@@ -0,0 +1,25 @@
+package vairiablesanddatatypes;
+
+public class ElseifStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		a=b;
+		if(a>b)
+		{
+			System.out.println("Exeute 1st statement");
+		}
+		else if(a==b)
+		{
+			System.out.println("execute 2nd statement");
+		}
+		else
+		{
+			System.out.println("execute 3rd statement");
+		}
+
+	}
+
+}
