1fe04289a13d910318cc6ebdcfc0c0af20aeb46d
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:48:38 +0530

    commit;

---
+++ BasicCoreJava/src/vairiablesanddatatypes/NestedIfstatement.java
@@ -0,0 +1,21 @@
+package vairiablesanddatatypes;
+
+public class NestedIfstatement {
+
+	public static void main(String[] args) {
+		
+int a=10;
+int b=20;
+int c=30;
+//login
+if(a>b)
+{
+//compose	
+	if(a>c)
+	{
+		System.out.println("a is greaest");
+	}
+}
+	}
+
+}
