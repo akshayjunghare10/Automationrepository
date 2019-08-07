648dd78c3a296ce3aba791b83bf9f6a9b860a960
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:52:37 +0530

    oertaors2 commit

---
+++ BasicCoreJava/src/vairiablesanddatatypes/LogicalOperators2.java
@@ -0,0 +1,19 @@
+package vairiablesanddatatypes;
+
+public class LogicalOperators2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int salary=50000;
+int mortgage=200000;
+if(salary>=50000 || mortgage>=200000)
+{
+	System.out.println("You are eligibal");
+}
+else
+{
+	System.out.println("You are not eligibal");
+}
+	}
+
+}
