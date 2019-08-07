47f1edb095f9630c245e0a2f8b08764bc3e93324
akshay <akshayjunghare10@gmail.com>
2019-08-07 23:50:59 +0530

    Array1 commit

--- BasicCoreJava/src/Arrays/Array1.java
+++ BasicCoreJava/src/Arrays/Array1.java
@@ -15,15 +15,19 @@ public class Array1 {
 		std[7]="anu";
 		std[8]="vinod";
 		
-		System.out.println(std[0]);
-		System.out.println(std[1]);
-		System.out.println(std[2]);
-		System.out.println(std[3]);
-		System.out.println(std[4]);
-		System.out.println(std[5]);
-		System.out.println(std[6]);
-		System.out.println(std[7]);
-		System.out.println(std[8]);
+		/*
+		 * System.out.println(std[0]); System.out.println(std[1]);
+		 * System.out.println(std[2]); System.out.println(std[3]);
+		 * System.out.println(std[4]); System.out.println(std[5]);
+		 * System.out.println(std[6]); System.out.println(std[7]);
+		 * System.out.println(std[8]);
+		 */
+	//for(int i=0;i<=8;i++)
+		
+	//	{
+//		System.out.println(std[i]);
+		
+//	}
 
 	}
 
