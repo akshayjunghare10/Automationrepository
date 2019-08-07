ef112fa10b0bab2efcfe0db24b864c819abdbc89
akshay <akshayjunghare10@gmail.com>
2019-08-07 23:44:32 +0530

    WhileLoop commit

---
+++ BasicCoreJava/src/loops/WhileLoop2.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class WhileLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int score=0;
+		while(score<=50000)
+		{
+			score=score+100;
+		}
+		System.out.println(score);
+		System.out.println("Testing");
+		
+	}
+
+}
