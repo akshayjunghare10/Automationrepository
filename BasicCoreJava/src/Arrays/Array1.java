fb7931f9536a1f0ef708f5fac834a3660a3e4b0b
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:42:23 +0530

    commit

---
+++ BasicCoreJava/src/Arrays/Array1.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		String[] std= new String[9];
+		std[0]="Pooja";
+		std[1]="akshay";
+		std[2]="pranali";
+		std[3]="daya";
+		std[4]="sumit";
+		std[5]="vijy";
+		std[6]="gauri";
+		std[7]="anu";
+		std[8]="vinod";
+		
+		System.out.println(std[0]);
+
+	}
+
+}
