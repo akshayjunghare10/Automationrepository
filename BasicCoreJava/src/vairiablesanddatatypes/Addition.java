e0d8ce597c0da766f7d494d4defb2cd8ae8d8315
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:56:38 +0530

    Varaiables Commit

--- BasicCoreJava/src/vairiablesanddatatypes/Addition.java
+++ BasicCoreJava/src/vairiablesanddatatypes/Addition.java
@@ -5,12 +5,24 @@ public class Addition {
 	
 	int a=10;
 	int b=20;
-	int c= a+b;
+	int c=50;
+	
 	int d=a-b;
 	
 	System.out.println("addition :"+c);
 	System.out.println("subtrion :"+d);
-			
+	 //OR
+	
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a/b);
+	System.out.println(a*b);
+	 a=b;
+	 b=c;
+	 b=a;
+	 System.out.println(a);
+	 System.out.println(b);
+	 System.out.println(c);
 }
 	
 	
