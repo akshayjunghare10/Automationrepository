683a61d7648b8fb4c3ff46504da454ec7e238752
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:54:31 +0530

    conditional staatment2 commit

---
+++ BasicCoreJava/src/vairiablesanddatatypes/CoditionalStatement2.java
@@ -0,0 +1,21 @@
+package vairiablesanddatatypes;
+
+public class CoditionalStatement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=30;
+		if(a<=b)
+		{
+			System.out.println("a is less than or equal to  b");
+			
+		}
+		else
+		{
+			System.out.println("a is not equal to b");
+		}
+
+	}
+
+}
