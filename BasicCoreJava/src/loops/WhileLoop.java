2f2db1547b637ea517bfecc30afa99b52a7db300
akshay <akshayjunghare10@gmail.com>
2019-08-07 23:45:33 +0530

    WhileLoop commit

---
+++ BasicCoreJava/src/loops/WhileLoop.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//1.initialization
+		int i=0;
+		while(i<10)//condition
+		{
+			System.out.println(i);
+			//increment
+			i++;
+		}
+	}
+
+}
