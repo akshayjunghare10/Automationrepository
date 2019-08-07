6b56af395492565e69dbce102078761730cefd8d
akshay <akshayjunghare10@gmail.com>
2019-08-07 19:56:00 +0530

    data types commit

--- BasicCoreJava/src/vairiablesanddatatypes/DataTypes.java
+++ BasicCoreJava/src/vairiablesanddatatypes/DataTypes.java
@@ -11,16 +11,21 @@ System.out.println(age);
 //Character value
 
 char gender='M';
+System.out.println(gender);
 
 //Decimal value
 float grade =8.7f;
+System.out.println(grade);
 double mrks=3.6;
+System.out.println(mrks);
 
 //Boolean value
 boolean status=true;
+System.out.println(status);
 
 //Strings
 String name="pooja";
+System.out.println(name);
 
 //For Integer values
 
