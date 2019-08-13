90240d7bcf8b80328260e7dcebaaf35dea79775b
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:23:52 +0530

    ExcelApplication commit

---
+++ BasicCoreJava/src/abstraction/ExcelApplicationFunction.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+public abstract class ExcelApplicationFunction 
+{
+	
+	public abstract void spreadsheet();
+	public abstract void save();
+	public abstract void update();
+	public abstract void delete();
+	public abstract void formatting();
+	public abstract void filter();
+
+}
