37aa4e7f2482c218c800fbe04abdd416a1833fa2
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:17:35 +0530

    CITI BANK COMMIT

---
+++ BasicCoreJava/src/nterface/CITI.java
@@ -0,0 +1,32 @@
+package nterface;
+
+public class CITI implements RBI
+{
+
+	@Override
+	public void savingAccount() {
+		System.out.println("Saving account in citi bank");
+	}
+
+	@Override
+	public void currentAccount() {
+		// TODO Auto-generated method stub
+		System.out.println("current ccount in citi bank");
+	}
+
+	@Override
+	public void debitcard() {
+		// TODO Auto-generated method stub
+		System.out.println("debit card of citi bank");
+	}
+
+	@Override
+	public void creditCard() {
+		
+		System.out.println("credit card of citi bNK");
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
