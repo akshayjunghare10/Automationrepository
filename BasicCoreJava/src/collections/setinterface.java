2f31ee58d2a96ef288a5e7e2f3a94d5f2b22b3b3
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:19:27 +0530

    setinterface commit

---
+++ BasicCoreJava/src/collections/setinterface.java
@@ -0,0 +1,54 @@
+package collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class setinterface {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+    /*Set<String> set=new HashSet<>();
+   
+    set.add("pooja");
+    set.add("akshay");
+    set.add("daya");
+    set.add("Gauri");
+    
+    //enhanced for loop
+    for(String s:set)
+    {
+   	 
+   	 System.out.println(s);
+   	 
+   	 
+    }*/
+    
+    //Treeset
+    Set<String> set=new TreeSet<>();
+    
+    set.add("pooja");
+    set.add("akshay");
+    set.add("daya");
+    set.add("Gauri");
+    
+    //enhanced for loop
+    for(String s:set)
+    {
+   	 
+   	 System.out.println(s);
+   	 
+   	 
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+	}
+
+}
