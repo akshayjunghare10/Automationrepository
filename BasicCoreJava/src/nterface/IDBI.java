a21c43f5be03297e887d6c2fd1b9312937d761a9
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:17:13 +0530

    IDBI COMMIT

---
+++ BasicCoreJava/src/nterface/IDBI.java
@@ -0,0 +1,32 @@
+package nterface;
+
+public class IDBI implements RBI
+	{
+
+	@Override
+	public void savingAccount() {
+		System.out.println("Saving account in idbi bank");
+	}
+
+	@Override
+	public void currentAccount() {
+		// TODO Auto-generated method stub
+		System.out.println("current ccount in idbi bank");
+	}
+
+	@Override
+	public void debitcard() {
+		// TODO Auto-generated method stub
+		System.out.println("debit card of idbi bank");
+	}
+
+	@Override
+	public void creditCard() {
+		
+		System.out.println("credit card of idbi bNK");
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
