e48a8c490dedae584dabff2e1b9eb4cc0e627dc2
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:24:19 +0530

    Demo commit

---
+++ BasicCoreJava/src/abstraction/Demo.java
@@ -0,0 +1,18 @@
+package abstraction;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		ExcelApplicationFunction sp=new Sprint3();
+		sp.spreadsheet();
+		sp.save();
+		sp.delete();
+		sp.update();
+		sp.formatting();
+		sp.filter();
+		
+
+	}
+
+}
