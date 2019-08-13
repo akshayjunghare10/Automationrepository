e65bfdd33e72271a45ccce8c9619833b51d061ac
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:15:46 +0530

    SBI Commit

---
+++ BasicCoreJava/src/nterface/SBI.java
@@ -0,0 +1,34 @@
+package nterface;
+
+public class SBI implements RBI
+{
+
+	@Override
+	public void savingAccount() {
+		System.out.println("Saving account in sbi bank");
+	}
+
+	@Override
+	public void currentAccount() {
+		// TODO Auto-generated method stub
+		System.out.println("current ccount in sbi bank");
+	}
+
+	@Override
+	public void debitcard() {
+		// TODO Auto-generated method stub
+		System.out.println("debit card of sbi bank");
+	}
+
+	@Override
+	public void creditCard() {
+		
+		System.out.println("credit card of sbi bank");
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+}
