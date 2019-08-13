e40ceb178d02ca2e48fc277386c0da830b2e4734
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:21:03 +0530

    Listinterface commit

---
+++ BasicCoreJava/src/collections/listinterface.java
@@ -0,0 +1,78 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import java.util.List;
+
+
+
+public class listinterface 
+{
+
+	/*public static void main(String[] args) 
+	{
+		List<String> list=new ArrayList<>();
+     list.add("pooja");
+     list.add("akshay");
+     list.add("daya");
+     list.add("Gauri");
+     
+     //enhanced for loop
+     for(String s:list)
+     {
+    	 
+    	 System.out.println(s);
+    	 
+    	 
+     }*/
+     
+     //linked list
+	public static void main(String[] args) 
+	{
+		List<String> list=new LinkedList<>();
+     list.add("pooja");
+     list.add("akshay");
+     list.add("daya");
+     list.add("Gauri");
+     
+     //enhanced for loop
+     for(String s:list)
+     {
+    	 
+    	 System.out.println(s);
+    	 
+    	 
+     }
+	}
+	
+	
+	//vector
+	
+	/*public static void main(String[] args) 
+	{
+		List<String> list=new Vector<>();
+     list.add("pooja");
+     list.add("akshay");
+     list.add("daya");
+     list.add("Gauri");
+     
+     //enhanced for loop
+     for(String s:list)
+     {
+    	 
+    	 System.out.println(s);
+    	 
+    	 
+     }
+	}*/
+	
+
+	
+	
+	
+	
+}
+	
+
+
