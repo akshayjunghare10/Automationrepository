0da682a62bfea784c0abf1d61ce01fd268395ef2
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:23:07 +0530

    sprint1 commit

---
+++ BasicCoreJava/src/abstraction/Sprint1.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+public abstract class Sprint1 extends ExcelApplicationFunction 
+{
+	public  void spreadsheet()
+	{
+		System.out.println("Spreadsheet");
+	}
+	public  void save()
+	{
+		System.out.println("save");
+	}
+}
