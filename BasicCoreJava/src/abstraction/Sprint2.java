2604136220d88876d14b9798d1c072cfd0ccb10b
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:22:31 +0530

    sprint2 commit

---
+++ BasicCoreJava/src/abstraction/Sprint2.java
@@ -0,0 +1,15 @@
+package abstraction;
+
+public abstract class Sprint2 extends Sprint1
+{
+
+	public void update()
+	{
+		System.out.println("update");
+	}
+	
+	public void delete()
+	{
+		System.out.println("delete");
+	}
+}
