72f896682512fc6534ffc267849cf1d0dad9a606
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:16:49 +0530

    RBI Commit

---
+++ BasicCoreJava/src/nterface/RBI.java
@@ -0,0 +1,10 @@
+package nterface;
+
+public interface RBI 
+{ 
+	//inside an interfce we can keep default method or static method.
+ public void savingAccount();
+ public void currentAccount();
+ public void debitcard();
+ public void creditCard();
+}
