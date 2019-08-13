630bbb8ebef7d225911007f1c40c4b2f8942e118
akshay <akshayjunghare10@gmail.com>
2019-08-13 23:20:32 +0530

    ObjectArray commit

---
+++ BasicCoreJava/src/collections/ObjectArray.java
@@ -0,0 +1,17 @@
+package collections;
+
+public class ObjectArray {
+
+	public static void main(String[] args) 
+	{
+	
+//different data types 
+		
+	Object[] obj=new Object[3];
+	obj[0]=10;
+	obj[1]=true;
+	obj[2]="akshay";
+	
+	}
+
+}
